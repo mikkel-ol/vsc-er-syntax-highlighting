3810f60598aefac75ffe3a2df0a02459515a4ec7
Michael Lundquist <michaellundquist7@gmail.com>
2020-01-10 15:01:16 -0500

    In this state this code can parse markdown into erd svg. However I still need to test this without using vscode from source

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-//import erd from 'erd'
+const erd = require('erd')
+let fs = require("fs");
+let path = require("path");
 
 /**
  * This function converts the erd syntax to svg of that erd
@@ -8,7 +10,9 @@
  */
 function erdToHTML( erdCode ){
     erd({ modelsText: erdCode, outputType: "html"});
-    return fs.readFileSync(path.join(__dirname, "./erd.html"), { encoding: 'utf8' });
+    let vscodeRootDir = path.dirname(require.main.filename);
+    let svg = fs.readFileSync(path.join( vscodeRootDir, "../erd.html"), { encoding: 'utf8' })
+    return svg;
 }
 
 module.exports.activate = () => {
@@ -17,9 +21,7 @@ module.exports.activate = () => {
             const highlight = md.options.highlight;
             md.options.highlight = (code, lang) => {
                 if (lang && lang.match(/\ber\b/i)) {
-                    console.log(code);
-                    return `HIHIHIH${code}`;
-                    //return erdToHTML(code);
+                    return erdToHTML(code);
                 }
                 return highlight(code, lang);
             };
